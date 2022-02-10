edb2c26c2de1048fa48154cd4850d80d11cefb3d
Paul Melnikow <email@paulmelnikow.com>
2022-02-10 11:24:31 -0600

    Bump rollup dependencies (#382)

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,24 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import url from '@rollup/plugin-url'
 
 import pkg from './package.json'
 
 export default {
   input: 'src/index.js',
   output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' },
   ],
   plugins: [
-    external(),
     url(),
     babel({
       exclude: 'node_modules/**',
+      babelHelpers: 'runtime',
     }),
     resolve(),
     commonjs(),
   ],
+  external: [/@babel\/runtime/],
 }
